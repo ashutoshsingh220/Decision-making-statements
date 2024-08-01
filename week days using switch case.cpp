400537f17a0d966c03f627bddb1368a1ac958ce0
ashutoshsingh220 <ashutosh220kharayat@gmail.com>
2024-08-01 22:07:19 +0530

    Add files via upload

---
+++ week days using switch case.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int choice;
+    cout<<"Enter the choice ";
+    cin>>choice;
+    switch(choice)
+    {
+    case 1:
+    {
+    cout<<"Monday"<<endl;
+}
+break;
+case 2:
+    {
+    cout<<"Tuesday"<<endl;
+}
+break;
+case 3:
+    {
+    cout<<"Wednesday"<<endl;
+}
+break;
+case 4:
+    {
+    cout<<"Thursday"<<endl;
+}
+break;
+case 5:
+    {
+    cout<<"Friday"<<endl;
+}
+break;
+case 6:
+    {
+    cout<<"Saturday"<<endl;
+}
+break;
+case 7:
+    {
+    cout<<"Sunday"<<endl;
+}
+break;
+default:
+{
+cout<<"Enter valid choice";
+}
+    }
+}
+
+// output
+/*Enter the choice 1
+Monday
+Enter the choice 3
+Wednesday
+Enter the choice 8                      //(as there are only 7 cases and the entered case is 8 thatswhy the output is this)
+Enter valid choice*/                
